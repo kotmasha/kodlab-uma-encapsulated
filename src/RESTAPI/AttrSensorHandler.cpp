d3e89e047e057d01da76f39f6802714810df2777
siqihuang <siqihuang2014@gmail.com>
2018-02-24 17:12:28 -0500

    update -- switch measurable to attr_sensor

--- src/RESTAPI/AttrSensorHandler.cpp
+++ src/RESTAPI/AttrSensorHandler.cpp
@@ -12,7 +12,7 @@ AttrSensorHandler::AttrSensorHandler(const string &handler_name) : UMARestHandle
 }
 
 void AttrSensorHandler::handle_create(UMARestRequest &request) {
-	//no post call available for measurable or measurable pair, as they are created by sensor
+	//no post call available for attr_sensor or attr_sensor pair, as they are created by sensor
 	const string request_url = request.get_request_url();
 	throw UMAException("Cannot handle POST " + request_url, UMAException::ERROR_LEVEL::ERROR, UMAException::ERROR_TYPE::BAD_OPERATION);
 }
@@ -20,12 +20,12 @@ void AttrSensorHandler::handle_create(UMARestRequest &request) {
 void AttrSensorHandler::handle_update(UMARestRequest &request) {
 	//only for test purpose
 	const string request_url = request.get_request_url();
-	if (request_url == "/UMA/object/measurable") {
-		update_measurable(request);
+	if (request_url == "/UMA/object/attr_sensor") {
+		update_attr_sensor(request);
 		return;
 	}
-	else if (request_url == "/UMA/object/measurable_pair") {
-		update_measurable_pair(request);
+	else if (request_url == "/UMA/object/attr_sensor_pair") {
+		update_attr_sensor_pair(request);
 		return;
 	}
 	throw UMAException("Cannot handle PUT " + request_url, UMAException::ERROR_LEVEL::ERROR, UMAException::ERROR_TYPE::BAD_OPERATION);
@@ -33,12 +33,12 @@ void AttrSensorHandler::handle_update(UMARestRequest &request) {
 
 void AttrSensorHandler::handle_read(UMARestRequest &request) {
 	const string request_url = request.get_request_url();
-	if (request_url == "/UMA/object/measurable") {
-		get_measurable(request);
+	if (request_url == "/UMA/object/attr_sensor") {
+		get_attr_sensor(request);
 		return;
 	}
-	else if (request_url == "/UMA/object/measurable_pair") {
-		get_measurable_pair(request);
+	else if (request_url == "/UMA/object/attr_sensor_pair") {
+		get_attr_sensor_pair(request);
 		return;
 	}
 
@@ -46,23 +46,23 @@ void AttrSensorHandler::handle_read(UMARestRequest &request) {
 }
 
 void AttrSensorHandler::handle_delete(UMARestRequest &request) {
-	//no delete call for measurable measurable_pair, they are handled in sensor
+	//no delete call for attr_sensor attr_sensor_pair, they are handled in sensor
 	const string request_url = request.get_request_url();
 	throw UMAException("Cannot handle DELETE " + request_url, UMAException::ERROR_LEVEL::ERROR, UMAException::ERROR_TYPE::BAD_OPERATION);
 }
 
-void AttrSensorHandler::get_measurable(UMARestRequest &request) {
+void AttrSensorHandler::get_attr_sensor(UMARestRequest &request) {
 	const string agent_id = request.get_string_query("agent_id");
 	const string snapshot_id = request.get_string_query("snapshot_id");
-	const string measurable_id = request.get_string_query("measurable_id");
+	const string attr_sensor_id = request.get_string_query("attr_sensor_id");
 
 	Agent *agent = World::getAgent(agent_id);
 	Snapshot *snapshot = agent->getSnapshot(snapshot_id);
-	AttrSensor *measurable = snapshot->getAttrSensor(measurable_id);
-	const double diag = measurable->getDiag();
-	const double old_diag = measurable->getOldDiag();
-	bool current = measurable->getCurrent();
-	bool isOriginPure = measurable->getIsOriginPure();
+	AttrSensor *attr_sensor = snapshot->getAttrSensor(attr_sensor_id);
+	const double diag = attr_sensor->getDiag();
+	const double old_diag = attr_sensor->getOldDiag();
+	bool current = attr_sensor->getCurrent();
+	bool isOriginPure = attr_sensor->getIsOriginPure();
 
 	request.set_message("AttrSensor info get");
 	request.set_data("diag", diag);
@@ -71,25 +71,25 @@ void AttrSensorHandler::get_measurable(UMARestRequest &request) {
 	request.set_data("isOriginPure", isOriginPure);
 }
 
-void AttrSensorHandler::update_measurable(UMARestRequest &request) {
+void AttrSensorHandler::update_attr_sensor(UMARestRequest &request) {
 	const string agent_id = request.get_string_query("agent_id");
 	const string snapshot_id = request.get_string_query("snapshot_id");
-	const string measurable_id = request.get_string_query("measurable_id");
+	const string attr_sensor_id = request.get_string_query("attr_sensor_id");
 
 	Agent *agent = World::getAgent(agent_id);
 	Snapshot *snapshot = agent->getSnapshot(snapshot_id);
-	AttrSensor *measurable = snapshot->getAttrSensor(measurable_id);
+	AttrSensor *attr_sensor = snapshot->getAttrSensor(attr_sensor_id);
 
 	if (request.check_data_field("diag")) {
 		const double diag = request.get_double_data("diag");
-		measurable->setDiag(diag);
+		attr_sensor->setDiag(diag);
 
 		request.set_message("Diag updated");
 		return;
 	}
 	else if (request.check_data_field("old_diag")) {
 		const double old_diag = request.get_double_data("old_diag");
-		measurable->setOldDiag(old_diag);
+		attr_sensor->setOldDiag(old_diag);
 
 		request.set_message("Old diag updated");
 		return;
@@ -98,43 +98,43 @@ void AttrSensorHandler::update_measurable(UMARestRequest &request) {
 	throw UMAException("The coming put request has nothing to update", UMAException::ERROR_LEVEL::WARN, UMAException::ERROR_TYPE::BAD_OPERATION);
 }
 
-void AttrSensorHandler::get_measurable_pair(UMARestRequest &request) {
+void AttrSensorHandler::get_attr_sensor_pair(UMARestRequest &request) {
 	const string agent_id = request.get_string_query("agent_id");
 	const string snapshot_id = request.get_string_query("snapshot_id");
-	const string measurable_id1 = request.get_string_query("measurable1");
-	const string measurable_id2 = request.get_string_query("measurable2");
+	const string attr_sensor_id1 = request.get_string_query("attr_sensor1");
+	const string attr_sensor_id2 = request.get_string_query("attr_sensor2");
 
 	Agent *agent = World::getAgent(agent_id);
 	Snapshot *snapshot = agent->getSnapshot(snapshot_id);
-	AttrSensorPair *measurable_pair = snapshot->getAttrSensorPair(measurable_id1, measurable_id2);
+	AttrSensorPair *attr_sensor_pair = snapshot->getAttrSensorPair(attr_sensor_id1, attr_sensor_id2);
 
-	const double w = measurable_pair->getW();
-	const bool d = measurable_pair->getD();
+	const double w = attr_sensor_pair->getW();
+	const bool d = attr_sensor_pair->getD();
 	request.set_message("AttrSensor pair info get");
 	request.set_data("w", w);
 	request.set_data("d", d);
 }
 
-void AttrSensorHandler::update_measurable_pair(UMARestRequest &request) {
+void AttrSensorHandler::update_attr_sensor_pair(UMARestRequest &request) {
 	const string agent_id = request.get_string_query("agent_id");
 	const string snapshot_id = request.get_string_query("snapshot_id");
-	const string measurable_id1 = request.get_string_query("measurable1");
-	const string measurable_id2 = request.get_string_query("measurable2");
+	const string attr_sensor_id1 = request.get_string_query("attr_sensor1");
+	const string attr_sensor_id2 = request.get_string_query("attr_sensor2");
 
 	Agent *agent = World::getAgent(agent_id);
 	Snapshot *snapshot = agent->getSnapshot(snapshot_id);
-	AttrSensorPair *measurable_pair = snapshot->getAttrSensorPair(measurable_id1, measurable_id2);
+	AttrSensorPair *attr_sensor_pair = snapshot->getAttrSensorPair(attr_sensor_id1, attr_sensor_id2);
 
 	if (request.check_data_field("w")) {
 		const double w = request.get_double_data("w");
-		measurable_pair->setW(w);
+		attr_sensor_pair->setW(w);
 
 		request.set_message("w updated");
 		return;
 	}
 	else if (request.check_data_field("d")) {
 		const bool d = request.get_bool_data("d");
-		measurable_pair->setD(d);
+		attr_sensor_pair->setD(d);
 
 		request.set_message("d updated");
 		return;
